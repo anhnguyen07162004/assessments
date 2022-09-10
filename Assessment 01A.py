b75b3acb1191d585eb091dfc0b413348e9cdb6d2
anhnguyen07162004 <112536201+anhnguyen07162004@users.noreply.github.com>
2022-09-09 18:05:30 -0600

    Create Assessment 01A.py

---
+++ Assessment 01A.py
@@ -0,0 +1,13 @@
+# Anh Nguyen
+# CSCI 102 – Section B
+# Assessment 01A
+# TA helped me with input function
+# Time: 5 minutes
+
+
+
+A = input("A> ")
+B = input("B> ")
+
+C = (int(A) ** 2) + (int(B) ** 2)
+print("OUTPUT ", C)
