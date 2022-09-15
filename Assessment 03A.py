0bb997040972322df5db3777c28ad35325c94531
anhnguyen07162004 <112536201+anhnguyen07162004@users.noreply.github.com>
2022-09-15 11:46:30 -0600

    Create Assessment 03A.py

---
+++ Assessment 03A.py
@@ -0,0 +1,18 @@
+# Anh Nguyen
+# CSCI 102 – Section B
+# Assessment 03A
+# References: Searched up what AFAIK stands for
+# Time: 10 minutes
+
+# Input
+print("Enter the Tweet or Message abbreviation to decode.")
+tweet = input("TWEET> ")
+
+if tweet == "BTW":
+  print("OUTPUT BTW = By the way")
+elif tweet == "DM":
+  print("OUTPUT DM = Direct message")
+elif tweet == "AFAIK":
+  print("OUTPUT AFAIK = As far as i know")
+elif tweet == "IDK":
+  print("OUTPUT IDK = I don't know")
